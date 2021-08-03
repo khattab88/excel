3da97d09f52e566dda2dd9a54e399f7ed865e36e
mahmoud.khattab <mahmoud.khattab@MAHMOUD-KHATTAB>
2021-08-03 10:45:24 +0200

    create first repository interface

--- API/Repositories/Interfaces/INationalParkRepository.cs
+++ API/Repositories/Interfaces/INationalParkRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,13 @@ namespace API.Repositories.Interfaces
 {
     public interface INationalParkRepository
     {
-
+        ICollection<NationalPark> GetNationalParks();
+        NationalPark GetNationalPark(int nationalParkId);
+        bool IsNationalParkExists(int id);
+        bool IsNationalParkExists(string name);
+        bool CreateNationlPark(NationalPark nationalPark);
+        bool UpdateNationalPark(NationalPark nationalPark);
+        bool DeleteNationalPark(NationalPark nationalPark);
+        bool Save();
     }
 }
--- API/Repositories/NationalParkRepository.cs
+++ API/Repositories/NationalParkRepository.cs
@@ -1,4 +1,5 @@
-﻿using API.Repositories.Interfaces;
+﻿using API.Models;
+using API.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,44 @@ namespace API.Repositories
 {
     public class NationalParkRepository : INationalParkRepository
     {
+        public bool CreateNationlPark(NationalPark nationalPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NationalPark GetNationalPark(int nationalParkId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsNationalParkExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsNationalParkExists(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
