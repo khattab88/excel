26138436782e80b31d642e29120174fde97c3215
mahmoud.khattab <mahmoud.khattab@MAHMOUD-KHATTAB>
2021-08-03 12:45:13 +0200

    create first controller

---
+++ API/Controllers/NationalParksController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Repositories.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParksController : Controller
+    {
+        private readonly INationalParkRepository _nationalParkRepo;
+        private readonly IMapper _mapper;
+
+        public NationalParksController(INationalParkRepository nationalParkRepo, IMapper mapper)
+        {
+            _nationalParkRepo = nationalParkRepo;
+            _mapper = mapper;
+        }
+    }
+}
