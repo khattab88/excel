50a2ba17df079e8747854e8820a3f6a092cf801a
Mahmoud Khattab <mahmoud.khattab@ExcelSystems.local>
2021-08-03 10:23:16 +0200

    initial commit

---
+++ API/Models/NationalPark.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class NationalPark
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime EstablishedAt { get; set; }
+    }
+}
---
+++ Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// using System.Comp
+
+namespace Models
+{
+    public class NationalPark
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
