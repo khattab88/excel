fca4fd18b9666ac2124f47e8cbd2ad334efce282
mahmoud.khattab <mahmoud.khattab@MAHMOUD-KHATTAB>
2021-08-04 11:42:45 +0200

    supress xml comments warnings

--- API/Models/NationalPark.cs
+++ API/Models/NationalPark.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace API.Models
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public class NationalPark
     {
         [Key]
@@ -21,4 +22,5 @@ namespace API.Models
 
         public DateTime EstablishedAt { get; set; }
     }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
